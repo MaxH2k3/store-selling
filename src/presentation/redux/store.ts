3e6c6426b15f3bcb097565d48ed3d73d3295d21d
luutrieuvi <luutrieuvi2003.com>
2024-04-09 21:18:51 +0700

    build structure

--- src/presentation/redux/store.ts
+++ src/presentation/redux/store.ts
@@ -1,8 +1,25 @@
-// src/redux/store.ts
+// import { configureStore } from '@reduxjs/toolkit';
+// import { useDispatch as useAppDispatch, useSelector as useAppSelector } from 'react-redux';
+// import { persistStore, persistReducer } from 'redux-persist';
+// import { rootPersistConfig, rootReducer } from './rootReducer';
 
-import { createStore } from 'redux';
-import rootReducer from './reducers';
+// // ----------------------------------------------------------------------
 
-const store = createStore(rootReducer);
+// const store = configureStore({
+//   reducer: persistReducer(rootPersistConfig, rootReducer),
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: false,
+//       immutableCheck: false,
+//     }),
+// });
 
-export default store;
+// const persistor = persistStore(store);
+
+// const { dispatch } = store;
+
+// const useSelector = useAppSelector;
+
+// const useDispatch = () => useAppDispatch();
+
+// export { store, persistor, dispatch, useSelector, useDispatch };
